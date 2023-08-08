f70658732a47ac268c06dec622ee16613ebcc84f
Nicolas Ricardo Maba <nicolas.rm2005@aluno.ifsc.edu.br>
2023-08-08 09:07:13 -0300

    validação cliente

--- cinema/src/main/java/control/ClienteDAO.java
+++ cinema/src/main/java/control/ClienteDAO.java
@@ -1,11 +1,12 @@
 package control;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import Interfaces.ICliente;
 import conexao.ConexaoMySql;
-import modelo.Funcionario;
 import modelo.Cliente;
 
 public class ClienteDAO implements ICliente {
@@ -33,21 +34,52 @@ public class ClienteDAO implements ICliente {
     
     
 
-    public boolean inserir(Cliente u, String sala, Integer cow, Integer col) {
+    public boolean inserir(Cliente u, String sala, Integer row, Integer col) {
     	
+    	
+
+    	String comando ="Select * from vendas inner join assento on assento_idassento = assento.idassento where col=?, row=?  ";
+    			PreparedStatement ps=null;
+    			ResultSet rs = null;
+    		try {
+				ps= conexao.prepareStatement(comando);
+				ps.setInt(1, Integer.valueOf (row));							
+				ps.setInt(2, Integer.valueOf (col));
+				rs = ps.executeQuery();
+			
+				if(rs!=null){
+					
+					
+				}
+				
+				
+				
+				
+				//ps.execute();
+				//ps.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+			}	
+    		
+    	return true;
     }
 
     public boolean remover(Cliente usua, Integer i, Integer j, Integer salaN) {
-               
+      
+    	
+    	
+    	
+    	return true;       
     }
 
     public boolean alterar(Cliente novoUsuario, Integer i, Integer j, Integer salaN) {
-                  
-    
+         return true;
     }
 
     public static Cliente listarUsuarios(Integer i, Integer j, Integer salaN) {
+    	Cliente
     	
+    	return cliente
         
     }
 }
