ae3b5b34dda02d0c771f3d72beb12408d0bcdd1c
Rafael <rafael.e01@aluno.ifsc.edu.br>
2023-10-03 08:41:58 -0300

    Exemplo com inserir de funcionario

---
+++ cinema/src/test/java/test/FuncionarioDAOTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import control.FuncionarioDAO;
+import modelo.Funcionario;
+
+public class FuncionarioDAOTest {
+	@Test 
+	private void testMetodoInserirFuncionario() {
+		// TODO Auto-generated method stub
+		Funcionario funcio = new Funcionario();
+		funcio.setNome("teste");
+		funcio.setCpf((long) 1111111);
+		funcio.setVendasDouble(100.0);
+		
+		FuncionarioDAO dao = FuncionarioDAO.getInstancia();
+		
+		///Mudar declaração da interface para retorrnar um objeto
+		//Funcionario fInserir = dao.inserir(funcio);
+		Boolean fInserir = dao.inserir(funcio);
+		
+		assertNotNull(fInserir);
+		assertEquals(true, fInserir);
+	}
+}
